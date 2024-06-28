8e542f010457f15c04477ab214857683ec4cc5a4
Nathan Chen <guangyu.chen40730-biph@basischina.com>
2024-06-28 14:24:41 +0800

    i

--- run.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Change with your weights file
-model = YOLO("weights.pt")
-
-# Change with your source file
-predictframe=model.predict(source="/Users/nathanchen/Downloads/11.mp4",show=True,save=True)
